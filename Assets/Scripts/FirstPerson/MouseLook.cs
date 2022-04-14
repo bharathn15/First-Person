284f55f75ebac5f4bb75dfe57ff700787eee4414
bharathn15 <bharath.bglr.15.com>
2022-04-14 11:57:38 +0530

    First Person Initial commit

---
+++ Assets/Scripts/FirstPerson/MouseLook.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    private float Mouse_Senesitivity = 100f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    void Update()
+    {
+
+        float MouseX = Input.GetAxis("Mouse X") * Time.deltaTime * Mouse_Senesitivity;
+        float MouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * Mouse_Senesitivity;
+
+        xRotation -= MouseY;
+        xRotation = Mathf.Clamp(xRotation, -75f,80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * MouseX);
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# First-Person
+
+______________________________________
+Unity - 2020.3.19f1
+______________________________________
+Visual Studio - 2019
+______________________________________
+Scenes - SampleScene 
+______________________________________
+Scripts - PlayerMovement, MouseLook
+______________________________________
+Use Mouse to Look around and WASD keys to move around
+
+
